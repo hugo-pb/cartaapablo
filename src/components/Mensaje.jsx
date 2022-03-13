f6154f3d3d688dc12b96aa377b5eeb0b4ea2ad47
hugo-pb <h.palomerabecerra@gmail.com>
2022-03-13 10:28:15 -0600

    experimento

--- src/components/Mensaje.jsx
+++ src/components/Mensaje.jsx
@@ -10,15 +10,19 @@ export default function Mensaje(){
         </h1>
 
 <Container>
-    <Row>
+    <Row style={{
+        display:"flex",
+        allignCotent: 'center',
+        justifyContent: 'center',
+    }}>
         <Col>
         <img src= {Ball} style={{height:'100px',
     marginTop: "6rem"}}/>
         </Col>
         <Col>
-        <p style={{marginTop: "para 6rem"}}> Tienes un 
-        gran potencial, y ahora que estas creciendo mas y mas 
-        responsabilidades     </p>
+        <p style={{marginTop: "6rem"}}> Tienes un 
+        gran potencial, y ahora que estas creciendo 
+        tendras mas y mas responsabilidades. </p>
         </Col>
     </Row>
 
